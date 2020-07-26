914a1e5063288062ffd8ed9c9530379a0a6bc4b8
Calvin Leung <kalvin807@gmail.com>
2020-07-27 00:24:29 +0800

    Prepare for github-page deployment

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import '../styles/index.css';
 import '../styles/timeline.css';
-import 'react-vertical-timeline-component/style.min.css';
 
 export default ({ Component, pageProps }) => <Component {...pageProps} />;
