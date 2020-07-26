24c7692a0ba94093713bce8432991683e4936978
Calvin Leung <kalvin807@gmail.com>
2020-07-27 01:36:20 +0800

    Fix basepath problem for hopefully script not loading fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withReactSvg = require('next-react-svg');
 const path = require('path');
+import dotenv from 'dotenv'
 
 module.exports = withReactSvg({
+  assetPrefix: process.env.GITHUB_PAGES ? '/kalpage' : '',
   include: path.resolve(__dirname, 'assets/svg'),
   webpack: function (config) {
     config.module.rules.push({
