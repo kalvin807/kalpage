7b297fd45f2e93dc4200893d806e6d9d553de138
Calvin Leung <kalvin807@gmail.com>
2020-07-27 00:48:32 +0800

    Fix tailwindcss not purgeing css

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 const withReactSvg = require('next-react-svg');
 const path = require('path');
-const withCss = require('@zeit/next-css');
 
-module.exports = withCss(
-  withReactSvg({
-    include: path.resolve(__dirname, 'assets/svg'),
-    webpack: function (config) {
-      config.module.rules.push({
-        test: /\.ya?ml$/,
-        use: 'js-yaml-loader',
-      });
-      return config;
-    },
-  })
-);
+module.exports = withReactSvg({
+  include: path.resolve(__dirname, 'assets/svg'),
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      use: 'js-yaml-loader',
+    });
+    return config;
+  },
+});
