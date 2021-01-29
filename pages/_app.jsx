23f169c070f4a0a386a779dec922f7a8c0e6e363
Calvin Leung <kalvin807@gmail.com>
2021-01-29 17:32:13 +0800

    removing obsolete code for static render

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,11 @@
 import useDarkMode from 'use-dark-mode';
-import { useEffect } from 'react';
 
 import '../styles/index.css';
 import '../styles/timeline.css';
 import '../styles/app.css';
 
 const App = ({ Component, pageProps }) => {
-  useEffect(() => {
-    document.body.className = (document.body.className ?? '').replace(
-      'no-js',
-      'js'
-    );
-  }, []);
-
   useDarkMode();
-
   return <Component {...pageProps} />;
 };
 
