82302c8c6522cc95f610da6c6927e96af232e705
Kal <46393179+kalvin807@users.noreply.github.com>
2021-08-11 17:40:31 +0800

    Feat/update pkg (#3)
    * update package and use icon lib
    
    * use cloudflare page now
    
    * not used now

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const theme = !darkMode.value ? light : dark;
 
   return (
-    <div>
+    <>
       <Head>
         <title>Home | Kal</title>
         <link rel="icon" href="/favicon.ico" />
@@ -25,7 +25,7 @@ const Home = () => {
       <div className="container mx-auto max-w-screen-lg p-8">
         <div className="py-16">
           <h1 className="text-2xl md:text-5xl font-bold ">
-            Hey, I'm Calvin Leung
+            {"Hey, I'm Calvin Leung"}
           </h1>
           <p>
             Welcome to my page. Here is small intro of my life as a dev,
@@ -35,12 +35,12 @@ const Home = () => {
         <div className="py-8">
           <h2 className="text-xl md:text-3xl font-bold">About me</h2>
           <p>
-            I'm a final year computer science student in Hong Kong. During
+            {`I'm a final year computer science student in Hong Kong. During
             school, I learnt a lot of computer theory, algorithm etc. At night,
             when I am free, I also do coding for fun. Trying out new things, new
             language to make my life easier. When I don't code, I like
             boardgames, tabletop rpg and video games. Occasionally, I am scout
-            and outdoor sports, like hiking and camping.
+            and outdoor sports, like hiking and camping.`}
           </p>
         </div>
         <div className="py-8">
@@ -49,7 +49,7 @@ const Home = () => {
         </div>
         <Footer />
       </div>
-    </div>
+    </>
   );
 };
 
