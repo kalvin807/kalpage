59a870a06acb88f5f228bf3c67091df5cc483c98
Kal <46393179+kalvin807@users.noreply.github.com>
2021-09-12 22:48:06 +0800

    feat/redesign (#7)
    * use pnpm and daisy theme
    
    * eslint and prettier
    
    * default template
    
    * remaked
    
    * 404
    
    * typos
    
    * remove unsed
    
    * ico
    
    * Readme
    
    * doggo spin

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import "tailwindcss/tailwind.css"
+import { ThemeProvider } from "next-themes"
+
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="data-theme" disableTransitionOnChange>
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
+
+export default App
