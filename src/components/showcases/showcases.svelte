ccdf7aa2b4432179fe3d7acfc235ed7d3306fd80
Kal.L <kalvin807@gmail.com>
2023-04-17 22:51:08 +0900

    ✨ feat(showcaseCard.svelte): add ShowcaseCard component ✨ feat(showcases.svelte): add ShowcaseCard component and showcase data

---
+++ src/components/showcases/showcases.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import ShowcaseCard from './showcaseCard.svelte';
+
+	const projects = [
+		{
+			title: 'GBFinder Omega - グラブル救援検索・マグナ',
+			description: 'Granblue Fantasy raid finder written in Go and React',
+			github: 'https://github.com/kalvin807/gbf-raid-finder',
+			web: 'https://gbf.kalvin.io',
+			gradient: 'bg-gradient-to-b from-sky-400 to-sky-200'
+		},
+		{
+			title: 'henkaku',
+			description: 'JS library to convert string between full-width and half-width',
+			github: 'https://github.com/kalvin807/henkaku',
+			gradient: 'bg-gradient-to-br from-indigo-300 to-purple-400'
+		},
+		{
+			title: 'guru-pu',
+			description: 'Discord bot to auto-assign roles',
+			github: 'https://github.com/kalvin807/guru-pu',
+			gradient: 'bg-gradient-to-r from-teal-200 to-lime-200'
+		},
+		{
+			title: 'akaina',
+			description: 'ML model to count the number of Magikarp in a youtube live stream',
+			github: 'https://github.com/kalvin807/akaina-detector',
+			gradient: 'bg-gradient-to-br from-rose-700 to-pink-600'
+		}
+	];
+</script>
+
+<div id="cards" class="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4">
+	{#each projects as project}
+		<ShowcaseCard {...project} />
+	{/each}
+</div>
