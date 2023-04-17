d9b273748a1e167a76d023286e148688f0e995f5
Kal.L <kalvin807@gmail.com>
2023-04-17 22:41:26 +0900

    ✨ feat(experiences): add experience component and data 🎨 refactor(page.svelte): remove unused code and import experiences and showcases components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,7 @@
 <script>
 	import doggo from '$lib/assets/doggo.png';
-	import Experiences from '../components/experienceCard/experiences.svelte';
-	import ShowcaseCard from '../components/showcaseCard.svelte';
-
-	const projects = [
-		{
-			title: 'GBFinder Omega - グラブル救援検索・マグナ',
-			description: 'Granblue Fantasy raid finder written in Go and React',
-			github: 'https://github.com/kalvin807/gbf-raid-finder',
-			web: 'https://gbf.kalvin.io',
-			gradient: 'bg-gradient-to-b from-sky-400 to-sky-200'
-		},
-		{
-			title: 'henkaku',
-			description: 'JS library to convert string between full-width and half-width',
-			github: 'https://github.com/kalvin807/henkaku',
-			gradient: 'bg-gradient-to-br from-indigo-300 to-purple-400'
-		},
-		{
-			title: 'guru-pu',
-			description: 'Discord bot to auto-assign roles',
-			github: 'https://github.com/kalvin807/guru-pu',
-			gradient: 'bg-gradient-to-r from-teal-200 to-lime-200'
-		},
-		{
-			title: 'akaina',
-			description: 'ML model to count the number of Magikarp in a youtube live stream',
-			github: 'https://github.com/kalvin807/akaina-detector',
-			gradient: 'bg-gradient-to-br from-rose-700 to-pink-600'
-		}
-	];
+	import Experiences from '../components/experiences/experiences.svelte';
+	import Showcases from '../components/showcases/showcases.svelte';
 </script>
 
 <div class="m-2 flex max-w-3xl flex-col gap-10 text-white md:m-0">
@@ -51,13 +23,7 @@
 
 	<div class="flex flex-col gap-6">
 		<h1 class="text-3xl font-bold md:text-4xl">Some stuff I made.</h1>
-		<div class="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4">
-			{#each projects as project}
-				<div class="flex-initial">
-					<ShowcaseCard {...project} />
-				</div>
-			{/each}
-		</div>
+		<Showcases />
 	</div>
 
 	<div class="flex flex-col gap-6">
