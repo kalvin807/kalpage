228fc0751fd2a591771f0e43acc7dac29068a412
Kal.L <kalvin807@gmail.com>
2023-04-17 22:51:08 +0900

    ✨ feat(showcaseCard.svelte): add ShowcaseCard component ✨ feat(showcases.svelte): add ShowcaseCard component and showcase data

--- src/components/showcases/showcaseCard.svelte
+++ src/components/showcases/showcaseCard.svelte
@@ -12,7 +12,7 @@
 	class={`h-full w-full transform rounded-xl p-1 transition-all hover:scale-[1.01] hover:text-white focus:outline-none focus:ring active:text-opacity-75 ${gradient}`}
 >
 	<div
-		class="relative flex h-full w-full flex-col gap-2 rounded-lg bg-slate-900 py-6 px-5 text-sm hover:bg-slate-900/80"
+		class="relative flex h-full w-full flex-col gap-2 rounded-lg bg-slate-900 px-5 py-6 text-sm hover:bg-slate-900/80"
 	>
 		<div class="flex flex-col gap-1">
 			<h3 class="text-md font-bold md:text-lg">{title}</h3>
@@ -21,14 +21,14 @@
 		<div class="flex flex-row justify-end gap-4">
 			{#if github}
 				<a href={github} target="_blank" rel="noopener noreferrer">
-					<button class="rounded p-2 transition-all hover:bg-white/20">
+					<button class="z-10 rounded p-2 transition-all hover:bg-white/20">
 						<Code class="h-5 w-5" />
 					</button>
 				</a>
 			{/if}
 			{#if web}
 				<a href={web} target="_blank" rel="noopener noreferrer">
-					<button class="rounded p-2 transition-all hover:bg-white/20">
+					<button class="z-10 rounded p-2 transition-all hover:bg-white/20">
 						<Globe2 class="h-5 w-5" />
 					</button>
 				</a>
