de2251b4b7a52202007b46e52d759cadb8ea9cad
Calvin Leung <kalvin807@gmail.com>
2024-10-23 17:49:28 +0900

    Add postcss.config.js for Tailwind CSS and Autoprefixer plugins

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import '../app.css';
+	let { children } = $props();
 	import Header from '../components/header.svelte';
 	import Footer from '../components/footer.svelte';
 </script>
 
 <Header />
 <div class=" flex flex-col content-center items-center gap-1">
-	<slot />
+	{@render children()}
 </div>
 <Footer />
