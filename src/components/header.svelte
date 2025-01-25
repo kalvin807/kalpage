65a24541b3ccd73b508c4f5b70e6c2d435d8729a
Calvin Leung <kalvin807@gmail.com>
2025-01-25 23:32:13 +0900

    bump pkgs

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -3,17 +3,17 @@
 </script>
 
 <div
-	class="supports-backdrop-blur:bg-white/95 sticky top-0 z-40 w-full flex-none bg-white backdrop-blur transition-colors duration-500 dark:border-slate-50/[0.06] dark:bg-slate-900/75 lg:z-50"
+	class="supports-backdrop-blur:bg-white/95 sticky top-0 z-40 w-full flex-none bg-white backdrop-blur-sm transition-colors duration-500 dark:border-slate-50/[0.06] dark:bg-slate-900/75 lg:z-50"
 >
 	<div class="mx-4 py-4 text-white lg:mx-0 lg:px-8">
 		<div class="relative flex items-center justify-end gap-2">
 			<a href="https://github.com/kalvin807">
-				<button class="rounded p-3 transition-all hover:bg-purple-500 hover:text-white">
+				<button class="rounded-sm p-3 transition-all hover:bg-purple-500 hover:text-white">
 					<Github class="hover:animate-wiggle" />
 				</button>
 			</a>
 			<a href="mailto:6q4zz1i46@relay.firefox.com">
-				<button class="rounded p-3 transition-all hover:bg-red-600 hover:text-white">
+				<button class="rounded-sm p-3 transition-all hover:bg-red-600 hover:text-white">
 					<Mail class="hover:animate-wiggle" />
 				</button>
 			</a>
--- src/components/showcases/showcaseCard.svelte
+++ src/components/showcases/showcaseCard.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <div
-	class={`h-full w-full transform rounded-xl p-1 transition-all hover:scale-[1.01] hover:text-white focus:outline-none focus:ring active:text-opacity-75 ${gradient}`}
+	class={`h-full w-full transform rounded-xl p-1 transition-all hover:scale-[1.01] hover:text-white focus:outline-hidden focus:ring-3 active:text-opacity-75 ${gradient}`}
 >
 	<div
 		class="relative flex h-full w-full flex-col gap-2 rounded-lg bg-slate-900 px-5 py-6 text-sm hover:bg-slate-900/80"
@@ -29,14 +29,14 @@
 		<div class="flex flex-row justify-end gap-4">
 			{#if github}
 				<a href={github} target="_blank" rel="noopener noreferrer">
-					<button class="z-10 rounded p-2 transition-all hover:bg-white/20">
+					<button class="z-10 rounded-sm p-2 transition-all hover:bg-white/20">
 						<Code class="h-5 w-5" />
 					</button>
 				</a>
 			{/if}
 			{#if web}
 				<a href={web} target="_blank" rel="noopener noreferrer">
-					<button class="z-10 rounded p-2 transition-all hover:bg-white/20">
+					<button class="z-10 rounded-sm p-2 transition-all hover:bg-white/20">
 						<Globe class="h-5 w-5" />
 					</button>
 				</a>
